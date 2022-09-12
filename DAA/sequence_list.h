86b3d0c1c058e74c3ebe33377caaec8a0beaf0e1
Flashingfire <952031165@qq.com>
2022-09-12 20:26:41 +0800

    继续完善顺序表的测试

--- DAA/sequence_list.h
+++ DAA/sequence_list.h
@@ -3,6 +3,7 @@
 #define ZYSEQUENCELIST_H_
 //顺序表的结构体定义
 #include<vector>
+#include<iostream>
 namespace zy {
 	template<class T>
 	class Sqlist {
@@ -16,6 +17,7 @@ namespace zy {
 		Sqlist(Sqlist&);
 		~Sqlist();
 		//顺序表的操作
+		void print();
 		//有序插入，顺序表中的元素递增有序排列，插入数字x后依然有序
 		void inorderInsert(T x);
 		//删除 下标为p的元素，成功返回1，否则返回0，并将被删除元素的值赋值给e
@@ -71,7 +73,8 @@ namespace zy {
 			}
 		}
 		//直接调用vector的insert函数
-		this->data.insert(lindex, x);
+		this->length++;
+		this->data.insert(this->data.begin()+lindex, x);
 	}
 	//删除 下标为p的元素，成功返回1，否则返回0，并将被删除元素的值赋值给e
 	template<class T>
@@ -81,14 +84,22 @@ namespace zy {
 			return 0;
 		}
 		try {
-			this->data.erase(p);
+			this->data.erase(this->data.begin()+p);
 		}
 		catch (...) {
 			e = this->data[p];
 			printf("delete fail");
 			throw;
 		}
+		this->length--;
 		return 1;
+	}
+
+	template<class T>
+	void Sqlist<T>::print() {
+		for (int i = 0; i < this->length; ++i)
+			std::cout << this->data[i] << ' ';
+		std::cout << std::endl;
 	}
 }
 
--- DAA/sequencelist_test.h
+++ DAA/sequencelist_test.h
@@ -5,14 +5,32 @@ namespace zy {
 	namespace sequence_list {
 		namespace test {
 			void test() {
-				Sqlist<int>a1;
-				std::vector<int>vc{ 1,2,3,4 };
-				Sqlist<int>a2(vc, 4);
-				Sqlist<int>a3(a2);
-				for (int i = 0; i < a3.length; i++) {
-					std::cout << a3.data[i] << " ";
-				}
-				std::cout << std::endl;
+				std::cout << "Sqlist test" << std::endl;
+				std::cout << "无参构造" << std::endl;
+				Sqlist<int>v1;
+				std::cout <<"Sqlist length:"<< v1.length << std::endl;
+				std::cout << "有参构造" << std::endl;
+				std::vector<int>vc{ 1,2,4,6 };
+				Sqlist<int>v2(vc, 4);
+				std::cout << "Sqlist data" << std::endl;
+				v2.print();
+				std::cout << "Sqlist length:" << v1.length << std::endl;
+				std::cout << "拷贝构造" << std::endl;
+				Sqlist<int>v3(v2);
+				std::cout << "Sqlist data" << std::endl;
+				v3.print();
+				std::cout << "Sqlist length:" << v1.length << std::endl;
+				
+				std::cout << "Sqlist 操作" << std::endl;
+				std::cout << "有序插入操作：此操作的前提是顺序表是有序增大的" << std::endl;
+				v3.inorderInsert(5);
+				std::cout << v3.length << std::endl;
+				std::cout << "result:" << std::endl;
+				v3.print();
+				std::cout << "删除 下标为p的元素，成功返回1，否则返回0，并将被删除元素的值赋值给e" << std::endl;
+				int e = 0;
+				v3.deleteElem(2,e);
+				v3.print();
 			}
 		}
 	}
